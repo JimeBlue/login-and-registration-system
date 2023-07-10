77eab840d8dffe40c6da5cd9a9a158b207b7da03
Jimena Miguez <j.miguez@airlst.com>
2023-07-10 16:14:22 +0200

    Create account page + layouts

---
+++ pages/account.vue
@@ -0,0 +1,38 @@
+<template>
+  <section>
+    <div>
+      <form>
+        <AppInputControl type="email" v-model="email"
+          >E-Mail Address</AppInputControl
+        >
+        <AppInputControl type="password" v-model="password"
+          >Password</AppInputControl
+        >
+        <AppButton type="submit">{{ isLogin ? "Login" : "Sign Up" }}</AppButton>
+        <AppButton type="button" @click="isLogin = !isLogin"
+          >Switch to {{ isLogin ? "Signup" : "Login" }}</AppButton
+        >
+      </form>
+    </div>
+  </section>
+</template>
+
+<script>
+import AppInputControl from "~/components/UI/AppInputControl.vue";
+import AppButton from "~/components/UI/AppButton.vue";
+export default {
+  name: "AccountPage",
+  layout: "default",
+  components: {
+    AppInputControl,
+    AppButton,
+  },
+  data() {
+    return {
+      isLogin: true,
+      email: "",
+      password: "",
+    };
+  },
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,38 +1,16 @@
 <template>
   <section>
-    <div>
-      <form>
-        <AppInputControl type="email" v-model="email"
-          >E-Mail Address</AppInputControl
-        >
-        <AppInputControl type="password" v-model="password"
-          >Password</AppInputControl
-        >
-        <AppButton type="submit">{{ isLogin ? "Login" : "Sign Up" }}</AppButton>
-        <AppButton type="button" @click="isLogin = !isLogin"
-          >Switch to {{ isLogin ? "Signup" : "Login" }}</AppButton
-        >
-      </form>
-    </div>
+    <h1>Welcome to the login and registration system</h1>
   </section>
 </template>
 
 <script>
-import AppInputControl from "~/components/UI/AppInputControl.vue";
-import AppButton from "~/components/UI/AppButton.vue";
 export default {
   name: "IndexPage",
-  layout: "admin",
-  components: {
-    AppInputControl,
-    AppButton,
-  },
+  layout: "guest",
+  components: {},
   data() {
-    return {
-      isLogin: true,
-      email: "",
-      password: "",
-    };
+    return {};
   },
 };
 </script>
